2b9a0709d4627a1d4847b40107acc54a9d999829
goodwiins <allocs16@gmail.com>
2020-02-24 02:28:56 +0100

    c

---
+++ c++programing/Variable Declaration and Initialization/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+int main(){
+    int slices;
+    cin >> slices;
+    cout << "you have " << slices << "slices of pizza." << std::endl;
+    printf("%i/n",slices);
+}
