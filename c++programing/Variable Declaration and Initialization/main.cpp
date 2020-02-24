8b6b524329450c1ef07266c4168b6f06a71cb299
goodwiins <allocs16@gmail.com>
2020-02-24 02:45:11 +0100

    name change

--- c++programing/Variable Declaration and Initialization/main.cpp
+++ c++programing/Variable Declaration and Initialization/main.cpp
@@ -3,7 +3,7 @@ using std::cout;
 using std::cin;
 int main(){
     int slices;
+    cout << "how many pizza's slices you eat?.\n";
     cin >> slices;
-    cout << "you have " << slices << "slices of pizza." << std::endl;
-    printf("%i/n",slices);
+    cout << "you have " << slices << " slices of pizza." << std::endl;
 }
