374f3b3f4dcd41b4985eae30d75c4f6f1e26eeb1
goodwiins <allocs16@gmail.com>
2020-02-25 03:42:18 +0100

    c

---
+++ c++programing/4-Data Type/6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+  // here we will use c++ style of string which is quite esaier than c style
+  string greeting = "hello";
+  string complite = greeting + " there";
+  cout << complite.length()   << endl;
+// classic c string style; it used a array that sored the chars whitin;
+  char name[] = "jack";
+
+
+}
