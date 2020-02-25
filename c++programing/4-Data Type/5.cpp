22c36ebd4834c486116251d31ee83f0134f1ad7e
goodwiins <allocs16@gmail.com>
2020-02-25 03:01:49 +0100

    c

---
+++ c++programing/4-Data Type/5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+
+std::cout << fmod(10,3.65) << '\n';
+std::cout << remainder(10,3) << '\n';
+std::cout << fmax(10,3) << '\n';
+std::cout << trunc(10,3) << '\n';
+std::cout << fmin(10,3) << '\n';
+}
