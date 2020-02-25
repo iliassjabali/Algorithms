5870c8c8289471354373a7115366d99aa373a21c
goodwiins <allocs16@gmail.com>
2020-02-25 02:35:36 +0100

    c

--- c++programing/4-Data Type/4.cpp
+++ c++programing/4-Data Type/4.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 // there many ways to decalation of const in c++ 1st is to put it right here before the main functon,
-#define PI 3.14 // called const or macro as well; 
+#define PI 3.14 // called const or macro as well;
 
 using namespace std;
 
@@ -8,7 +8,14 @@ using namespace std;
 int main(int argc, char const *argv[]) {
 // also u can define it here as classic way to decalar the const in this way
   const float Euler = 2; // right u got my joke 2.718
+
+// also we there is another type of const is enum
+ enum { y = 100 };
   cout << Euler << endl;
   cout << PI << endl;
+  cout << y << endl;
 }
-// the result is 2 and 3.14
+// the results are:
+//2
+//3.14
+//100 
